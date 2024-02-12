2b5df80b32eba7e254b15d4039815d25211ca3ab
aleksem07 <aleksem07@gmail.com>
2024-02-12 13:47:33 +0300

    feat: add part 06

--- src/06-mocking-node-api/index.test.ts
+++ src/06-mocking-node-api/index.test.ts
@@ -2,6 +2,13 @@
 import { readFileAsynchronously, doStuffByTimeout, doStuffByInterval } from '.';
 
 describe('doStuffByTimeout', () => {
+  const callback = jest.fn();
+  const timeout = 222;
+
+  beforeEach(() => {
+    jest.spyOn(global, 'setTimeout');
+  });
+
   beforeAll(() => {
     jest.useFakeTimers();
   });
@@ -11,12 +18,13 @@ describe('doStuffByTimeout', () => {
   });
 
   test('should set timeout with provided callback and timeout', () => {
-    // Write your test here
+    doStuffByTimeout(callback, timeout);
+    expect(setTimeout).toHaveBeenCalledWith(callback, timeout);
   });
 
   test('should call callback only after timeout', () => {
     const callback = jest.fn();
-    const timeout = 1000;
+    const timeout = 100;
     doStuffByTimeout(callback, timeout);
     expect(callback).not.toBeCalled();
     jest.advanceTimersByTime(timeout);
@@ -25,6 +33,17 @@ describe('doStuffByTimeout', () => {
 });
 
 describe('doStuffByInterval', () => {
+  const callback = jest.fn();
+  const interval = 222;
+
+  beforeEach(() => {
+    jest.spyOn(global, 'setInterval');
+  });
+
+  afterEach(() => {
+    jest.clearAllTimers();
+  });
+
   beforeAll(() => {
     jest.useFakeTimers();
   });
@@ -34,13 +53,12 @@ describe('doStuffByInterval', () => {
   });
 
   test('should set interval with provided callback and timeout', () => {
-    // Write your test here
+    doStuffByInterval(callback, interval);
+    expect(setInterval).toHaveBeenCalledWith(callback, interval);
   });
 
   test('should call callback multiple times after multiple intervals', () => {
-    const callback = jest.fn();
-    const interval = 1000;
-    const numOfCalls = 3;
+    const numOfCalls = 2;
     doStuffByInterval(callback, interval);
     expect(callback).not.toBeCalled();
     jest.advanceTimersByTime(interval * numOfCalls);
@@ -54,7 +72,7 @@ describe('readFileAsynchronously', () => {
   });
 
   test('should return null if file does not exist', async () => {
-    const nonExistentFile = 'non-existent-file.txt';
+    const nonExistentFile = 'non-exist-file.txt';
     const result = await readFileAsynchronously(nonExistentFile);
     expect(result).toBeNull();
   });
